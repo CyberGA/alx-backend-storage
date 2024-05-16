1a9f9aa249dc99a44473382bc2e8e05f3060cd22
CyberGA <etomu.joshua@gmail.com>
2024-05-15 19:17:51 -0700

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for a redis class and methods"""
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 from uuid import uuid4
 
 
@@ -20,3 +20,23 @@ class Cache:
         stored_key = str(uuid4())
         self._redis.set(stored_key, data)
         return stored_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        converts the data to desired format"""
+        val = self._redis.get(key)
+        if fn:
+            val = fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """parametrize Cache.get with the correct conversion function"""
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """parametrize Cache.get with the correct conversion function"""
+        try:
+            return self.get(key, fn=lambda x: int(x.decode("utf-8")))
+        except Exception:
+            return 0
