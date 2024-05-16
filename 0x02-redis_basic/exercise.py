6f46ec63ba61396d9628066b6f966f5139fc5f61
CyberGA <etomu.joshua@gmail.com>
2024-05-15 19:36:50 -0700

    task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,6 +22,22 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    store the history of inputs and
+    outputs for a particular function"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wraps the decorated function and"""
+        inputs = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", inputs)
+        outputs = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", outputs)
+        return outputs
+    return wrapper
+
+
 class Cache:
     """declares a redis cache class"""
     def __init__(self):
@@ -32,6 +48,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data arg and
         returns the key after storing it"""
