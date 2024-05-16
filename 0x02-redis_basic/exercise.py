d5040668dee70967ae197b08a66862b3ecc26df6
CyberGA <etomu.joshua@gmail.com>
2024-05-15 18:53:44 -0700

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,12 +11,12 @@ class Cache:
         """
         store a redis client instance and
         flush the instance """
-        self.__redis = redis.Redis(host='localhost', port=6379, db=0)
-        self__redis.flushdb()
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self_redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data arg and
         returns the key after storing it"""
         stored_key = str(uuid4())
-        self.__redis.set(stored_key, data)
+        self._redis.set(stored_key, data)
         return stored_key
